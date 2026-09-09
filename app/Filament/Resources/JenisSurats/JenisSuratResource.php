987f4a1abf656a88f75036d2816345ec062b64ec
Ahmad Rofiqi Firdaus <ahmad.rofiqi.firdaus@gmail.com>
2026-09-09 22:52:37 +0700

    changing icon of jenis surat & klasifikasi surat menu

--- app/Filament/Resources/JenisSurats/JenisSuratResource.php
+++ app/Filament/Resources/JenisSurats/JenisSuratResource.php
@@ -18,7 +18,7 @@ class JenisSuratResource extends Resource
 {
     protected static ?string $model = JenisSurat::class;
 
-    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedDocumentText;
 
     protected static string|\UnitEnum|null $navigationGroup = 'Master Data';
 
--- app/Filament/Resources/Klasifikasis/KlasifikasiResource.php
+++ app/Filament/Resources/Klasifikasis/KlasifikasiResource.php
@@ -18,7 +18,7 @@ class KlasifikasiResource extends Resource
 {
     protected static ?string $model = Klasifikasi::class;
 
-    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedTag;
 
     protected static string|\UnitEnum|null $navigationGroup = 'Master Data';
 
