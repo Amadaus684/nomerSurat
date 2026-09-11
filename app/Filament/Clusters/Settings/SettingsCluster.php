e5e3eb04bbf75a1e758172104712fc01fcf8c21c
Ahmad Rofiqi Firdaus <ahmad.rofiqi.firdaus@gmail.com>
2026-09-11 19:55:35 +0700

    change sub navigation position to top

--- app/Filament/Clusters/Settings/SettingsCluster.php
+++ app/Filament/Clusters/Settings/SettingsCluster.php
@@ -5,10 +5,13 @@ namespace App\Filament\Clusters\Settings;
 use BackedEnum;
 use Filament\Clusters\Cluster;
 use Filament\Support\Icons\Heroicon;
+use Filament\Pages\Enums\SubNavigationPosition;
 
 class SettingsCluster extends Cluster
 {
     protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedCog6Tooth;
+
+    protected static ?SubNavigationPosition $subNavigationPosition = SubNavigationPosition::Top;
 
     protected static ?string $navigationLabel = 'Settings';
 
